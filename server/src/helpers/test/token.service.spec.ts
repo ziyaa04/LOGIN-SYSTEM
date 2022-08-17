310a8afc84c5746036e479fb86d195ab9b34bf31
Ziya <ziyakerimli04@gmail.com>
2022-08-17 22:29:39 +0300

    finsish token service tests

--- server/src/helpers/test/token.service.spec.ts
+++ server/src/helpers/test/token.service.spec.ts
@@ -4,6 +4,7 @@ import { TokenService } from '../token.service';
 import { Test } from '@nestjs/testing';
 import { JwtModule, JwtSignOptions } from '@nestjs/jwt';
 import { matchersWithOptions } from 'jest-json-schema';
+import { User } from '../../models/user.model';
 
 describe('Token service tests', () => {
   let tokenService: TokenService;
@@ -17,6 +18,9 @@ describe('Token service tests', () => {
       imports: [
         JwtModule.register({
           secret: 'some secret',
+          signOptions: {
+            expiresIn: '24h',
+          },
         }),
       ],
       providers: [TokenService],
@@ -51,24 +55,74 @@ describe('Token service tests', () => {
       );
     });
   });
-  describe('signAccessToken  function', () => {
-    let payload: ISignPayload;
+  describe('VerifyToken function', () => {
+    let token: string;
     beforeEach(() => {
-      payload = {
+      token =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImV4YW1wbGVAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.WN_Gv1LJq5JVCgXVKUl7He0t8FWom4BSAPceABeGwzk';
+    });
+    test('to be defined', () => {
+      expect(tokenService.verifyToken).toBeDefined();
+      expect(tokenService.verifyToken).not.toBeUndefined();
+    });
+    test('schema match result', () => {
+      const schema = {
+        type: 'object',
+        properties: {
+          email: {
+            type: 'string',
+          },
+          iat: {
+            type: 'number',
+          },
+        },
+      };
+      expect(tokenService.verifyToken(token)).toMatchSchema(schema);
+    });
+    test('check result', () => {
+      expect(tokenService.verifyToken(token)).toHaveProperty(
+        'email',
+        'example@gmail.com',
+      );
+    });
+  });
+  describe('GeneratePayload function', () => {
+    let user;
+    beforeEach(() => {
+      user = {
         email: 'example@gmail.com',
         isActivated: true,
         roles: [RolesEnum.USER],
       };
     });
     test('to be defined', () => {
-      expect(tokenService.signAccessToken).toBeDefined();
-      expect(tokenService.signAccessToken).not.toBeUndefined();
+      expect(tokenService.generatePayload).toBeDefined();
+      expect(tokenService.generatePayload).not.toBeUndefined();
     });
-    test('schema match result', () => {
-      const schema = {
-        type: 'string',
+    test('to match scheme', () => {
+      const scheme = {
+        type: 'object',
+        properties: {
+          email: {
+            type: 'string',
+          },
+          isActivated: {
+            type: 'boolean',
+          },
+          roles: {
+            type: 'array',
+          },
+        },
       };
-      expect(tokenService.signAccessToken(payload)).toMatchSchema(schema);
+      expect(tokenService.generatePayload(user)).toMatchSchema(scheme);
+    });
+    test('check result', () => {
+      const res = {
+        email: 'example@gmail.com',
+        isActivated: true,
+        roles: [RolesEnum.USER],
+      };
+      expect(tokenService.generatePayload(user)).toEqual(res);
     });
   });
 });
